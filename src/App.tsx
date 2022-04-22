45de5ae0b7e28bb6792a85b92ab15ac8be846b79
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-04-22 16:35:19 +0700

    add router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route,Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 
 function App() {
 
   return (
     <div className="App">
-     <HomePage />
+       <Routes>
+      <Route path="/" element={ <HomePage />}></Route>
+       </Routes>
+     
     </div>
   )
 }

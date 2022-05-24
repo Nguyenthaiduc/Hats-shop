33aaaffb3c2ae3852d935163bb75f05d75672d76
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-24 17:06:27 +0700

    add context

---
+++ src/context/userContext.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import IAccount from '../interfaces/IAccount';
+
+const UserContext = React.createContext<Omit<IAccount, 'password'> | null>(
+	null
+);
+export default UserContext;

0fb016e60b738f3b637675c98d44e060b39bbea4
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-24 17:15:45 +0700

    add firebase utils

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,27 +1,85 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import { initializeApp } from 'firebase/app';
+
+import {
+	addDoc,
+	collection,
+	doc,
+	getDoc,
+	getFirestore,
+	setDoc,
+} from 'firebase/firestore';
+
+//import auth from 'firebase/auth';
+import {
+	createUserWithEmailAndPassword,
+	getAuth,
+	setPersistence,
+	browserSessionPersistence,
+	signOut,
+	updateProfile,
+} from 'firebase/auth';
+import IAccount from '../interfaces/IAccount';
 
 const config = {
-    apiKey: "AIzaSyAEiPjsmQn54RrY-gzenRh16Zg8IRB8TUA",
-    authDomain: "shop-hats-db.firebaseapp.com",
-    projectId: "shop-hats-db",
-    storageBucket: "shop-hats-db.appspot.com",
-    messagingSenderId: "722884031778",
-    appId: "1:722884031778:web:f7b053089afe41f168617b",
-    measurementId: "G-TRC848P4JV"
-  };
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+	authDomain: 'clothshop-b6cc2.firebaseapp.com',
+	projectId: 'clothshop-b6cc2',
+	storageBucket: 'clothshop-b6cc2.appspot.com',
+	messagingSenderId: '644045758042',
+	appId: '1:644045758042:web:5249b90958c8338bbc0263',
+	measurementId: 'G-ZCFMX14DKK',
+};
+const app = initializeApp(config);
+const initauth = getAuth(app);
+const auth = getAuth();
 
-  firebase.initializeApp(config)
-//   export const database = firebase.database();
-  export const firestore = firebase.firestore();
-  export const auth = firebase.auth()
+export const createUser = async (
+	account: Omit<IAccount, 'displayName'> & { displayName?: string }
+) => {
+	try {
+		const userCredential = await createUserWithEmailAndPassword(
+			initauth,
+			account.email,
+			account.password
+		);
+		const user = userCredential.user;
+		if (account.displayName)
+			updateProfile(user, { displayName: account.displayName });
+	} catch (err) {
+		console.log(err);
+	}
+};
+export const ggSignOut = () =>
+	signOut(auth)
+		.then(() => {
+			localStorage.removeItem('myclothToken');
+			// Sign-out successful.
+		})
+		.catch((error) => {
+			// An error happened.
+		});
+export const createUserProfile = async (user: any) => {
+	try {
+		const docRef = doc(db, 'users', `${user.uid}`);
+		const snapShot = await getDoc(docRef);
+		await setDoc(docRef, {
+			displayName: user.displayName,
+			email: user.email,
+		});
+	} catch (e) {
+		console.error('Error adding document: ', e);
+	}
+};
+export const setPersistenceFirebase = setPersistence(
+	auth,
+	browserSessionPersistence
+)
+	.then(() => {
+		return null;
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+export const db = getFirestore(app);
 
-  const provider = new firebase.auth.GoogleAuthProvider()
-  provider.setCustomParameters({
-      prompt: "select_account"
-  })
-
-  export const signInWithGoogle = () => auth.signInWithPopup(provider)
-
-  export default firebase
+export default initauth;
